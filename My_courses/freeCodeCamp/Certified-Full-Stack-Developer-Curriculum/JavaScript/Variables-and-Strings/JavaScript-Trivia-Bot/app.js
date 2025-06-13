771521bb9cd756c998a7cfe90404fa57d52c5771
Piotr Sasin <piotr.sasin.biz@outlook.com>
2025-06-13 23:04:54 +0200

    Update app.js

--- My_courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/JavaScript/Variables-and-Strings/JavaScript-Trivia-Bot/app.js
+++ My_courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/JavaScript/Variables-and-Strings/JavaScript-Trivia-Bot/app.js
@@ -1,1 +1,27 @@
-.
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+botName = "Neo";
+let botLocation;
+botLocation = "Matrix"
+let favoriteLanguage;
+favoriteLanguage = "JavaScript"
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact;
+codingFact = ("The fun fact about " + favoriteLanguage + " is that, it can be the easiest language to learn and on the same time, it can be the hardest language to learn.");
+
+console.log(codingFact);
+
+codingFact = ("The another fun fact about " + favoriteLanguage + " is that, Ryan Dahl who invented JavaScript, apologized for his inventions many years later.");
+
+console.log(codingFact);
+
+codingFact = ("Next fun fact about " + favoriteLanguage + " is that, JavaScript doesn't have a moscot, but they are working with David Cronenberg to create one.");
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
