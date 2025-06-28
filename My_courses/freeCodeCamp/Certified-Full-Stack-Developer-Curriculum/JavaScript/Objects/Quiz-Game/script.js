be27e84679f0f025d9675081cb6db41ed9faf66b
Piotr Sasin <piotr.sasin.biz@outlook.com>
2025-06-28 12:59:39 +0200

    Update script.js

--- My_courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/JavaScript/Objects/Quiz-Game/script.js
+++ My_courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/JavaScript/Objects/Quiz-Game/script.js
@@ -41,7 +41,7 @@ let randomQuestion;
 
 function getRandomQuestion (questions) {
     const questionsArray = [question1.question, question2.question, question3.question, question4.question, question5.question];
-    randomQuestion = questionsArray[Math.round(Math.random() * 5)];
+    randomQuestion = questionsArray[Math.floor(Math.random() * 5)];
     return randomQuestion;
 }
 
@@ -49,39 +49,67 @@ let randomChoice;
 
 function getRandomComputerChoice (choices) {
     if (randomQuestion == question1.question) {
-        randomChoice = question1.choices[Math.round(Math.random() * 5)];
+        randomChoice = question1.choices[Math.floor(Math.random() * 5)];
         return randomChoice;
-    } else {}
-
-    if (randomQuestion == question2.question) {
-        randomChoice = question2.choices[Math.round(Math.random() * 5)];
+    
+    } else if (randomQuestion == question2.question) {
+        randomChoice = question2.choices[Math.floor(Math.random() * 5)];
         return randomChoice;
-    } else {}
-
-    if (randomQuestion == question3.question) {
-        randomChoice = question3.choices[Math.round(Math.random() * 5)];
+    
+    } else if (randomQuestion == question3.question) {
+        randomChoice = question3.choices[Math.floor(Math.random() * 5)];
         return randomChoice;
-    } else {}
-
-    if (randomQuestion == question4.question) {
-        randomChoice = question4.choices[Math.round(Math.random() * 5)];
+    
+    } else if (randomQuestion == question4.question) {
+        randomChoice = question4.choices[Math.floor(Math.random() * 5)];
         return randomChoice;
-    } else {}
-
-    if (randomQuestion == question5.question) {
-        randomChoice = question5.choices[Math.round(Math.random() * 5)];
+    
+    } else if (randomQuestion == question5.question) {
+        randomChoice = question5.choices[Math.floor(Math.random() * 5)];
         return randomChoice;
-    } else {}
+    
+    } else {
+        return `Error - if...else doesn't work in getRandomComputerChoice function`;
+    }
 }
 
-let result;
-
 function getResults (randomQuestion, randomChoice) {
-    if () {
-
-        return `The computer's choice is correct!`;
+    if (randomQuestion == question1.question) {
+        if (randomChoice == question1.answer) {
+            return `The computer's choice is correct!`;
+        } else {
+            return `The computer's choice is wrong. The correct answer is: ${question1.answer}`;
+        }
+    
+    } else if (randomQuestion == question2.question) {
+        if (randomChoice == question2.answer) {
+            return `The computer's choice is correct!`;
+        } else {
+            return `The computer's choice is wrong. The correct answer is: ${question2.answer}`;
+        }
+    
+    } else if (randomQuestion == question3.question) {
+        if (randomChoice == question3.answer) {
+            return `The computer's choice is correct!`;
+        } else {
+            return `The computer's choice is wrong. The correct answer is: ${question3.answer}`;
+        }
+    
+    } else if (randomQuestion == question4.question) {
+        if (randomChoice == question4.answer) {
+            return `The computer's choice is correct!`;
+        } else {
+            return `The computer's choice is wrong. The correct answer is: ${question4.answer}`;
+        }
+    
+    } else if (randomQuestion == question5.question) {
+        if (randomChoice == question5.answer) {
+            return `The computer's choice is correct!`;
+        } else {
+            return `The computer's choice is wrong. The correct answer is: ${question5.answer}`;
+        }
     } else {
-        return `The computer's choice is wrong. The correct answer is: ${}`
+        return `Error - if...else doesn't work in getResults function`;
     }
 }
 
@@ -90,4 +118,3 @@ console.log(getRandomQuestion());
 console.log(getRandomComputerChoice());
 
 console.log(getResults());
-
