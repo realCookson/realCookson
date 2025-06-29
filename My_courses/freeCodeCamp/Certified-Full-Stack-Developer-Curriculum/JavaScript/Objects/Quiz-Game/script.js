6a6d4694b4f5a0fec489e3675b3d6d3b77360e72
Piotr Sasin <piotr.sasin.biz@outlook.com>
2025-06-29 20:49:52 +0200

    Update script.js

--- My_courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/JavaScript/Objects/Quiz-Game/script.js
+++ My_courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/JavaScript/Objects/Quiz-Game/script.js
@@ -3,118 +3,67 @@ const questions = [] ;
 const question1 = {
     category: `closed question`,
     question: `What is the capital of Poland?`,
-    choices: [`Łódź`, `Warsaw`, `Gdańsk`, `Kraków`],
+    choices: [`Łódź`, `Warsaw`, `Gdańsk`],
     answer: `Warsaw`,
 };
 
 const question2 = {
     category: `closed question`,
     question: `What is the capital of France?`,
-    choices: [`Rennes`, `Marseille`, `Paris`, `Lyon`],
+    choices: [`Rennes`, `Marseille`, `Paris`],
     answer: `Paris`,
 };
 
 const question3 = {
     category: `closed question`,
     question: `What is the capital of Spain?`,
-    choices: [`Toledo`, `Madrid`, `Valencia`, `Bilbao`],
+    choices: [`Toledo`, `Madrid`, `Valencia`],
     answer: `Madrid`,
 };
 
 const question4 = {
     category: `closed question`,
     question: `What is the capital of Deutschland?`,
-    choices: [`Berlin`, `Hamburg`, `Frankfurt`, `Monachium`],
+    choices: [`Berlin`, `Hamburg`, `Frankfurt`],
     answer: `Berlin`,
 };
 
 const question5 = {
     category: `open question`,
     question: `With how many countries Poland is neighboring?`,
-    choices: [5, 6, 7, 8],
-    answer: 7,
+    choices: [`5`, `6`, `7`],
+    answer: `7`,
 };
 
 questions.push(question1, question2, question3, question4, question5);
 
 let randomQuestion;
 
+// 8. You should have a function named getRandomQuestion that takes an array of questions as a parameter and returns a random question object from the array.
 function getRandomQuestion (questions) {
-    const questionsArray = [question1.question, question2.question, question3.question, question4.question, question5.question];
-    randomQuestion = questionsArray[Math.floor(Math.random() * 5)];
+    randomQuestion = questions[Math.floor(Math.random() * questions.length)];
     return randomQuestion;
 }
 
+console.log(getRandomQuestion(questions));
+
 let randomChoice;
 
+// 9. You should have a function named getRandomComputerChoice that takes the array of the available choices as a parameter, and returns a random answer to the selected question.
 function getRandomComputerChoice (choices) {
-    if (randomQuestion == question1.question) {
-        randomChoice = question1.choices[Math.floor(Math.random() * 5)];
-        return randomChoice;
-    
-    } else if (randomQuestion == question2.question) {
-        randomChoice = question2.choices[Math.floor(Math.random() * 5)];
-        return randomChoice;
-    
-    } else if (randomQuestion == question3.question) {
-        randomChoice = question3.choices[Math.floor(Math.random() * 5)];
-        return randomChoice;
-    
-    } else if (randomQuestion == question4.question) {
-        randomChoice = question4.choices[Math.floor(Math.random() * 5)];
-        return randomChoice;
-    
-    } else if (randomQuestion == question5.question) {
-        randomChoice = question5.choices[Math.floor(Math.random() * 5)];
-        return randomChoice;
-    
-    } else {
-        return `Error - if...else doesn't work in getRandomComputerChoice function`;
-    }
+    randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
 }
 
+console.log(getRandomComputerChoice(randomQuestion.choices));
+
+// 11. Your getResults function should take the question object as the first parameter and the computer's choice as the second parameter.
 function getResults (randomQuestion, randomChoice) {
-    if (randomQuestion == question1.question) {
-        if (randomChoice == question1.answer) {
-            return `The computer's choice is correct!`;
-        } else {
-            return `The computer's choice is wrong. The correct answer is: ${question1.answer}`;
-        }
-    
-    } else if (randomQuestion == question2.question) {
-        if (randomChoice == question2.answer) {
-            return `The computer's choice is correct!`;
-        } else {
-            return `The computer's choice is wrong. The correct answer is: ${question2.answer}`;
-        }
-    
-    } else if (randomQuestion == question3.question) {
-        if (randomChoice == question3.answer) {
-            return `The computer's choice is correct!`;
-        } else {
-            return `The computer's choice is wrong. The correct answer is: ${question3.answer}`;
-        }
-    
-    } else if (randomQuestion == question4.question) {
-        if (randomChoice == question4.answer) {
-            return `The computer's choice is correct!`;
-        } else {
-            return `The computer's choice is wrong. The correct answer is: ${question4.answer}`;
-        }
-    
-    } else if (randomQuestion == question5.question) {
-        if (randomChoice == question5.answer) {
-            return `The computer's choice is correct!`;
-        } else {
-            return `The computer's choice is wrong. The correct answer is: ${question5.answer}`;
-        }
+    if (randomChoice === randomQuestion.answer) {
+        return `The computer's choice is correct!`;
     } else {
-        return `Error - if...else doesn't work in getResults function`;
+        return `The computer's choice is wrong. The correct answer is: ${randomQuestion.answer}`;
     }
 }
 
-console.log(getRandomQuestion());
-
-console.log(getRandomComputerChoice());
-
-console.log(getResults());
+console.log(getResults(randomQuestion, randomChoice));
