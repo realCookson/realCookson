f479131cf50aada783277a2e05555e65bc984c26
Piotr Sasin <piotr.sasin.biz@outlook.com>
2025-07-04 22:58:16 +0200

    Update script.js

--- My_courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/JavaScript/JavaScript-Fundamentals-Review/Pyramid-Generator/script.js
+++ My_courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/JavaScript/JavaScript-Fundamentals-Review/Pyramid-Generator/script.js
@@ -2,8 +2,11 @@ function pyramid(string, integer, boolean) {
     let pyramid = "\n";
     if (boolean === false) {
         let numOfStrings = 1;
-        let numOfSpaces = 3;
+        let numOfSpaces = (2 * integer - 1)/2;
         for (let i = 0; i < integer; i++) {
+            if (numOfSpaces <= 0) {
+                numOfSpaces = 0;
+            }
             pyramid = pyramid + " ".repeat(numOfSpaces) + string.repeat(numOfStrings) + "\n";
             numOfStrings += 2;
             numOfSpaces -= 1;
@@ -23,3 +26,5 @@ function pyramid(string, integer, boolean) {
 console.log(pyramid("o", 4, false));
 
 console.log(pyramid("p", 5, true));
+
+console.log(pyramid("N", 10, false));
