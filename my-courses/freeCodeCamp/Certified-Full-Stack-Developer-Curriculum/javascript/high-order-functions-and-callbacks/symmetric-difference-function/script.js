c63e33657a44bb4436f1f5a867f70b900477d45e
Piotr Sasin <piotr.sasin.biz@outlook.com>
2025-11-25 11:13:42 +0100

    Update script.js

--- my-courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/javascript/high-order-functions-and-callbacks/symmetric-difference-function/script.js
+++ my-courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/javascript/high-order-functions-and-callbacks/symmetric-difference-function/script.js
@@ -1,16 +1,24 @@
 function diffArray(arr1, arr2) {
-  let mergedArr = [];
-  for (let i = 0; i < arr1.length; i++) {
-    mergedArr.push(arr1[i]);
-  }
-  for (let i = 0; i < arr2.length; i++) {
-    mergedArr.push(arr2[i]);
-  }
-  console.log(mergedArr);
-  let filteredArr = mergedArr.filter();
-  return filteredArr;
+  let filteredArrayA = [];
+  let filteredArrayB = [];
+  filteredArrayA = arr1.filter(item => {
+    if (arr2.includes(item) === false) {
+      return item;
+    }
+  });
+  filteredArrayB = arr2.filter(item => {
+    if (arr1.includes(item) === false) {
+      return item;
+    }
+  });
+  console.log(filteredArrayA);
+  console.log(filteredArrayB);
+  let result = filteredArrayA.concat(filteredArrayB);
+  return result;
 }
 
 console.log(diffArray(["diamond", "stick", "apple"], ["stick", "emerald", "bread"]));
 
 console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+
+console.log(diffArray(["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"], ["andesite", "grass", "dirt", "dead shrub"]));
