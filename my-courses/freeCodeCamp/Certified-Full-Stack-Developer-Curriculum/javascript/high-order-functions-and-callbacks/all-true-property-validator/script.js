0f5842ed4e758ef096ea0f610b9ddd090de4a652
Piotr Sasin <piotr.sasin.biz@outlook.com>
2025-12-02 15:28:56 +0100

    Create script.js

---
+++ my-courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/javascript/high-order-functions-and-callbacks/all-true-property-validator/script.js
@@ -0,0 +1,29 @@
+function truthCheck(collection, pre) {
+  let result = undefined;
+  for (const obj of collection) {
+    if (obj[pre] == false || Number.isNaN(obj[pre]) === true) {
+      result = false;
+      break
+    } else {
+      result = true;
+      continue
+    }
+  }
+  return result;
+}
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot")); //should return false
+
+console.log(truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "name")); //should return true
+
+console.log(truthCheck([{name: "Pikachu", number: 25, caught: 3}, {name: "Togepi", number: 175, caught: 1}, {name: "MissingNo", number: NaN, caught: 0}], "number"));
+//should return false
+
+console.log(truthCheck([{name: "Quincy", username: "QuincyLarson"}, {name: "Naomi", username: "nhcarrigan"}, {name: "Camperbot"}], "username"));
+//should return false
+
+console.log(truthCheck([{name: "freeCodeCamp", users: [{name: "Quincy"}, {name: "Naomi"}]}, {name: "Code Radio", users: [{name: "Camperbot"}]}, {name: "", users: []}], "users"));
+//should return true
+
+console.log(truthCheck([{id: 1, data: {url: "https://freecodecamp.org", name: "freeCodeCamp"}}, {id: 2, data: {url: "https://coderadio.freecodecamp.org/", name: "CodeRadio"}}, {id: null, data: {}}], "id"));
+//should return false
