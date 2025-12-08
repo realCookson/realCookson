40be51aac739d5c8eb38d6e247b904717282f8d3
Piotr Sasin <piotr.sasin.biz@outlook.com>
2025-12-08 15:44:27 +0100

    Create script.js

---
+++ my-courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/javascript/javascript-and-accessibility/theme-switcher/script.js
@@ -0,0 +1,9 @@
+const themes = [
+  { name: "light", message: "x - Light theme is on!" },
+  { name: "dark", message: "x - Dark theme is on!" },
+  { name: "high contrast", message: "x - High contrast theme is on!" },
+  { name: "matrix", message: "x - Matrix theme is on!" },
+];
+
+//TODO create message for themes
+
