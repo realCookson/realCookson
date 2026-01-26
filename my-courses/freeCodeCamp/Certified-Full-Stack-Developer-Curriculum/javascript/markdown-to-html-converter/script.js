565beac001cf8d3936ea49c8a5b26852b44d83a3
Piotr Sasin <piotr.sasin.biz@outlook.com>
2026-01-26 14:35:30 +0100

    Update script.js

--- my-courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/javascript/markdown-to-html-converter/script.js
+++ my-courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/javascript/markdown-to-html-converter/script.js
@@ -5,49 +5,30 @@ const htmlPreview = document.getElementById("preview");
 const headingOneRegex = /^#\s(.*)/gm; //Markdown: # heading 1
 const headingTwoRegex = /^##\s(.*)/gm; //Markdown: ## heading 2
 const headingThreeRegex = /^###\s(.*)/gm; //Markdown: ### heading 3
-const boldRegex = /^(\*\*|__)(.*?)\1$/gm; //Markdown: **bold text** or __bold text__
-const italicRegex = /^(\*|_)(.*?)\1$/gm; //Markdown: *italic text* or _italic text_
-const imgRegex = /^!\[(.*?)\]\((.*?)\)/gm; //Markdown: ![alt-text](image-source)
-const linkRegex = /^\[(.*?)\]\((.*?)\)/gm; //Markdown: [link text](URL)
-const quoteRegex = /^>\s(.*)/gm; //Markdown: > quote
+const boldRegex = /(\*\*|__)(.*?)\1/gm; //Markdown: **bold text** or __bold text__
+const italicRegex = /(\*|_)(.*?)\1/gm; //Markdown: *italic text* or _italic text_
+const imgRegex = /!\[(.*?)\]\((.*?)\)/gm; //Markdown: ![alt-text](image-source)
+const linkRegex = /\[(.*?)\]\((.*?)\)/gm; //Markdown: [link text](URL)
+const quoteRegex = /^> (.*)/gm; //Markdown: > quote
 
 function convertMarkdown() {
   const userInput = markdownInput.value;
-  if (headingOneRegex.test(userInput)) {
-    htmlOutput.textContent = userInput.replace(headingOneRegex, "<h1>$1</h1>");
-  } else if (headingTwoRegex.test(userInput)) {
-    htmlOutput.textContent = userInput.replace(headingTwoRegex, "<h2>$1</h2>");
-  } else if (headingThreeRegex.test(userInput)) {
-    htmlOutput.textContent = userInput.replace(
-      headingThreeRegex,
-      "<h3>$1</h3>",
-    );
-  } else if (boldRegex.test(userInput)) {
-    htmlOutput.textContent = userInput.replace(
-      boldRegex,
-      "<strong>$2</strong>",
-    );
-  } else if (italicRegex.test(userInput)) {
-    htmlOutput.textContent = userInput.replace(italicRegex, "<em>$2</em>");
-  } else if (imgRegex.test(userInput)) {
-    htmlOutput.textContent = userInput.replace(
-      imgRegex,
-      `<img alt="$1" src="$2">`,
-    );
-  } else if (linkRegex.test(userInput)) {
-    htmlOutput.textContent = userInput.replace(
-      linkRegex,
-      `<a href="$2">$1</a>`,
-    );
-  } else if (quoteRegex.test(userInput)) {
-    htmlOutput.textContent = userInput.replace(
-      quoteRegex,
-      "<blockquote>$1</blockquote>",
-    );
+  let result = userInput;
+  result = result.replaceAll(headingOneRegex, "<h1>$1</h1>");
+  result = result.replaceAll(headingTwoRegex, "<h2>$1</h2>");
+  result = result.replaceAll(headingThreeRegex, "<h3>$1</h3>");
+  result = result.replaceAll(boldRegex, "<strong>$2</strong>");
+  result = result.replaceAll(italicRegex, "<em>$2</em>");
+  result = result.replaceAll(imgRegex, `<img alt="$1" src="$2">`);
+  result = result.replaceAll(linkRegex, `<a href="$2">$1</a>`);
+  result = result.replaceAll(quoteRegex, "<blockquote>$1</blockquote>");
+  console.log(result);
+  if (result === userInput) {
+    htmlOutput.textContent = ""; //basically if conversion wasn't made, then don't send output
   } else {
-    htmlOutput.textContent = "";
+    htmlOutput.textContent = result;
   }
-  htmlPreview.innerHTML = htmlOutput.textContent;
+  return (htmlPreview.innerHTML = htmlOutput.textContent);
 }
 
 markdownInput.addEventListener("input", () => {
