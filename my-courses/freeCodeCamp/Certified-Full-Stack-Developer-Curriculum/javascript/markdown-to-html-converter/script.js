02480f240db70b0e124cada033c13088f8caae95
Piotr Sasin <piotr.sasin.biz@outlook.com>
2026-01-28 11:20:57 +0100

    Update script.js

--- my-courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/javascript/markdown-to-html-converter/script.js
+++ my-courses/freeCodeCamp/Certified-Full-Stack-Developer-Curriculum/javascript/markdown-to-html-converter/script.js
@@ -14,20 +14,40 @@ const quoteRegex = /^> (.*)/gm; //Markdown: > quote
 function convertMarkdown() {
   const userInput = markdownInput.value;
   let result = userInput;
-  result = result.replaceAll(headingOneRegex, "<h1>$1</h1>");
-  result = result.replaceAll(headingTwoRegex, "<h2>$1</h2>");
-  result = result.replaceAll(headingThreeRegex, "<h3>$1</h3>");
-  result = result.replaceAll(boldRegex, "<strong>$2</strong>");
-  result = result.replaceAll(italicRegex, "<em>$2</em>");
-  result = result.replaceAll(imgRegex, `<img alt="$1" src="$2">`);
-  result = result.replaceAll(linkRegex, `<a href="$2">$1</a>`);
-  result = result.replaceAll(quoteRegex, "<blockquote>$1</blockquote>");
-  if (result === userInput) {
-    htmlOutput.textContent = ""; //basically if conversion wasn't made, then don't send output
-  } else {
-    htmlOutput.textContent = result;
+
+  if (headingOneRegex.test(result)) {
+    result = result.replaceAll(headingOneRegex, "<h1>$1</h1>");
   }
-  console.log(`htmlOutput: ${htmlOutput.textContent}`);
+
+  if (headingTwoRegex.test(result)) {
+    result = result.replaceAll(headingTwoRegex, "<h2>$1</h2>");
+  }
+
+  if (headingThreeRegex.test(result)) {
+    result = result.replaceAll(headingThreeRegex, "<h3>$1</h3>");
+  }
+
+  if (boldRegex.test(result)) {
+    result = result.replaceAll(boldRegex, "<strong>$2</strong>");
+  }
+
+  if (italicRegex.test(result)) {
+    result = result.replaceAll(italicRegex, "<em>$2</em>");
+  }
+
+  if (imgRegex.test(result)) {
+    result = result.replaceAll(imgRegex, `<img alt="$1" src="$2">`);
+  }
+
+  if (linkRegex.test(result)) {
+    result = result.replaceAll(linkRegex, `<a href="$2">$1</a>`);
+  }
+
+  if (quoteRegex.test(result)) {
+    result = result.replaceAll(quoteRegex, "<blockquote>$1</blockquote>");
+  }
+
+  htmlOutput.textContent = result;
   return (htmlPreview.innerHTML = htmlOutput.textContent);
 }
 
